d7e16e0b91ffa840f6a7c134f0c69d540f3be4f0
Amit Parmar <amit.parmar@ext.dpworld.com>
2026-02-14 13:22:13 +0530

    My First flashcard app commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import MyComponent from './Components/MyComponent'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>My App Component</h1>
+      <MyComponent />
+    </div>)
+}
+
+export default App
